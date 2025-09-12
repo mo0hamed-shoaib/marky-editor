a696a60f7f72df4a7c548080e2c2ec3cc54da2a4
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-09-13 02:59:21 +0300

    feat: add robots.txt configuration for search engines

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/', '/_next/'],
+    },
+    sitemap: 'https://marky-app.vercel.app/sitemap.xml',
+  }
+}
