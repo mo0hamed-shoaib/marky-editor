617bd325848078c35e7e8f7af3b4e520a3b337ec
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-09-17 21:03:20 +0300

    cleanup: remove the remaining features from notes app and remove the urls to them

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,13 +11,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${baseUrl}/dashboard`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/editor-x`,
+      url: `${baseUrl}/learn`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
